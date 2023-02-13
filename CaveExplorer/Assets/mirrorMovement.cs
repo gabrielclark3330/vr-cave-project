8c2fbfb72e3f2f3ebf4488fe72c7240e864f0c5b
Gabriel Clark <gabrielclark3330@gmail.com>
2023-02-13 16:51:52 -0600

    Some sort of movement mechanic

---
+++ CaveExplorer/Assets/mirrorMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirrorMovement : MonoBehaviour
+{
+
+    public Transform playerTransform;
+    private Vector3 pickaxeOrigin;
+    private Vector3 playerOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickaxeOrigin = gameObject.transform.position;
+        playerOrigin = playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerTransform.position = playerOrigin + (pickaxeOrigin-gameObject.transform.position);
+    }
+}
