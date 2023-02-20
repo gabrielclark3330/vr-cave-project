c687104c8d56b826c89bfb180cffb2535cbc88fb
Riddock Moran <57115079+PCao2@users.noreply.github.com>
2023-02-20 17:36:33 -0600

    Fixed flicker bug

--- CaveExplorer/Assets/climbingLogic.cs
+++ CaveExplorer/Assets/climbingLogic.cs
@@ -8,45 +8,86 @@ namespace Valve.VR.InteractionSystem
 {
     public class climbingLogic : MonoBehaviour
     {
+        public GameObject vrCamera;
         public Transform playerLAxeTransform;
         public Transform playerRAxeTransform;
         private Vector3 pickaxeOnCollidePosition;
-        private Vector3 playerOnCollidePosition;
         public ClimberHand RightHand;
         public ClimberHand LeftHand;
-
+        public SteamVR_Action_Boolean triggerAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabPinch");
+        private Hand inputHand;
         public SteamVR_Action_Boolean teleportAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Teleport");
         public GameObject RightAxe;
         public GameObject LeftAxe;
 
         private Player player = null;
+        private bool lHandTrigger = false;
+        private bool rHandTrigger = false;
+        private float timeSpentNotGrabbing = 0;
+        private bool playerOnGround = true;
 
         // Start is called before the first frame update
         void Start()
         {
-            player = InteractionSystem.Player.instance;
+            player = Player.instance;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (LeftHand.TouchedCount > 0 && !LeftHand.grabbing)
+            updateTriggerVariables();
+
+            if (LeftHand.TouchedCount > 0 && !LeftHand.grabbing && lHandTrigger)
             {
                 RightHand.grabbing = false;
                 LeftHand.grabbing = true;
                 pickaxeOnCollidePosition = playerLAxeTransform.position;
-                playerOnCollidePosition = gameObject.transform.position;
-                Debug.Log("Left");
             }
-            if (RightHand.TouchedCount > 0 && !RightHand.grabbing)
+            if (RightHand.TouchedCount > 0 && !RightHand.grabbing && rHandTrigger)
             {
                 LeftHand.grabbing = false;
                 RightHand.grabbing = true;
                 pickaxeOnCollidePosition = playerRAxeTransform.position;
-                playerOnCollidePosition = gameObject.transform.position;
-                Debug.Log("Right");
             }
+            /*
+            if (!lHandTrigger)
+            {
+                LeftHand.grabbing = false;
+            }
+            if (!rHandTrigger)
+            {
+                RightHand.grabbing = false;
+            }*/
 
+            updateAxeActive();
+
+            if (RightHand.grabbing)
+            {
+                gameObject.transform.position += (pickaxeOnCollidePosition - playerRAxeTransform.position);
+                playerOnGround = false;
+            }
+            if (LeftHand.grabbing)
+            {
+                gameObject.transform.position += (pickaxeOnCollidePosition - playerLAxeTransform.position);
+                playerOnGround = false;
+            }
+            /*
+            else if (!LeftHand.grabbing && !RightHand.grabbing)
+            {
+                timeSpentNotGrabbing += Time.deltaTime;
+                if (vrCamera.transform.position.y < 1.8288) // 6ft in meters
+                {
+                    playerOnGround = true;
+                }
+                if (!playerOnGround)
+                {
+                    gameObject.transform.position -= new Vector3(0, timeSpentNotGrabbing*-9.81f, 0); // gravitational constant
+                }
+            }*/
+        }
+
+        private void updateAxeActive()
+        {
             bool teleportButtonDown = false;
             foreach (Hand hand in player.hands)
             {
@@ -59,22 +100,11 @@ namespace Valve.VR.InteractionSystem
             {
                 RightAxe.SetActive(false);
                 LeftAxe.SetActive(false);
-                //RightHand.grabbing = false;
-                //LeftHand.grabbing = false;
             }
             else
             {
                 RightAxe.SetActive(true);
                 LeftAxe.SetActive(true);
-            }
-            
-            if (RightHand.grabbing)
-            {
-                gameObject.transform.position = playerOnCollidePosition + (pickaxeOnCollidePosition - playerRAxeTransform.position);
-            }
-            if (LeftHand.grabbing)
-            {
-                gameObject.transform.position = playerOnCollidePosition + (pickaxeOnCollidePosition - playerLAxeTransform.position);
             }
         }
 
@@ -88,6 +118,47 @@ namespace Valve.VR.InteractionSystem
             {
                 return teleportAction.GetState(hand.handType);
             }
+        }
+
+        private void updateTriggerVariables()
+        {
+            if (triggerAction.stateDown) // If holding down trigger
+            {
+                inputHand = inputChecker();
+                if (inputHand == Player.instance.leftHand)
+                {
+                    lHandTrigger = true;
+                }
+                if (inputHand == Player.instance.rightHand)
+                {
+                    rHandTrigger = true;
+                }
+            }
+            if (triggerAction.stateUp)
+            {
+                inputHand = inputChecker();
+                if (inputHand == Player.instance.leftHand)
+                {
+                    lHandTrigger = false;
+                }
+                if (inputHand == Player.instance.rightHand)
+                {
+                    rHandTrigger = false;
+                }
+            }
+        }
+
+        private Hand inputChecker()
+        {
+            if (triggerAction.activeDevice == SteamVR_Input_Sources.RightHand)
+            {
+                inputHand = Player.instance.rightHand;
+            }
+            else if (triggerAction.activeDevice == SteamVR_Input_Sources.LeftHand)
+            {
+                inputHand = Player.instance.leftHand;
+            }
+            return inputHand;
         }
     }
 }
